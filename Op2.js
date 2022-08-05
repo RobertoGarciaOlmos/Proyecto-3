79a14d50437dac35e55fcb370364507bb1c341c2
Robert <roberto.garcia.olmos@gmail.com>
2022-08-04 23:20:07 -0500

    SEMIFINAL

--- Op2.js
+++ Op2.js
@@ -1,106 +1,4 @@
-const monedaUsd = ["usd"];
-const monedaEur = ["eur"];
-const monedaCad = ["cad"];
-const monedaBrl = ["brl"];
-const monedaMxn = ["mxn"];
-const monedaUyu = ["uyu"];
 const ctx = document.getElementById('myChart');
-let valores = [];
-const datasets = [{
-    label: 'Currency vs USD',
-    backgroundColor: [
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 99, 132, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ],
-    borderWidth: 1
-}]
-
-
-
-
-// DATOS GRAFICAS
-
-// USD-USD
-
-const getMonedaUsa = async() => {
-    valores = [];
-    const { data: { usd }, } = await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${monedaUsd[0]}.min.json`);
-    valores.push(usd);
-    datasets[0].data = valores
-    myChart.update();
-}
-
-
-
-// EUR-USD
-
-const getMonedaEur = async() => {
-    valores = [];
-    const { data: { eur }, } = await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${monedaEur[0]}.min.json`);
-    valores.push(eur);
-    datasets[0].data = valores
-    myChart.update();
-}
-
-
-
-
-// USD-CAD
-
-const getMonedaCad = async() => {
-    valores = [];
-    const { data: { cad }, } = await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${monedaCad[0]}.min.json`);
-    valores.push(cad);
-    datasets[0].data = valores
-    myChart.update();
-}
-
-
-
-
-// USD-BRL
-
-const getMonedaBrl = async() => {
-    valores = [];
-    const { data: { brl }, } = await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/${monedaBrl[0]}.min.json`);
-    valores.push(brl);
-    datasets[0].data = valores
-    myChart.update();
-}
-
-getMonedaUsa()
-getMonedaEur()
-getMonedaCad()
-getMonedaBrl()
-
-
-// GRAFICA
-
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['USD', 'EUR', 'CAD', 'BRL ', 'MNX', 'UYU'],
-        datasets
-    },
-
-});
-
-
-
-
-// TABLA VALORES COMPARACION APIS
 
 
 // USD-EURO
@@ -123,8 +21,10 @@ let url1 = [
     "https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/usd/eur.json"
 ]
 
+const fin = fetchUSDEUR(url1).then(respuesta => respuesta.json()).then((data1) => { console.log(data1.eur) })
 fetchUSDEUR(url1).then(respuesta => respuesta.json()).then((data1) => { document.getElementById("EURO").innerHTML = (`1 USD vale ${+(data1.eur).toFixed(2)} EUR`) })
 
+const Final = console.log(isNaN(fin))
 
 // USD-CANADA
 const fetchUSDcad = async(links, obj) => {
@@ -139,17 +39,17 @@ const fetchUSDcad = async(links, obj) => {
     return response
 }
 
+
 let url2 = [
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/cad.min.json",
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/cad.json",
     "https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/usd/cad.min.json",
     "https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/usd/cad.json"
 ]
-
+fetchUSDcad(url2).then(res => res.json()).then((data2) => { console.log(data2) })
 fetchUSDEUR(url2).then(respuesta => respuesta.json()).then((data2) => { document.getElementById("CAD").innerHTML = (`1 USD vale ${ +(data2.cad).toFixed(2)} CAD`) })
 
 // USD-Brazil
-
 const fetchUSDbrl = async(links, obj) => {
     let response;
     for (let link of links) {
@@ -162,6 +62,7 @@ const fetchUSDbrl = async(links, obj) => {
     return response
 }
 
+
 let url3 = [
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/brl.min.json",
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/brl.json",
@@ -169,7 +70,8 @@ let url3 = [
     "https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/usd/brl.json"
 ]
 
-fetchUSDbrl(url3).then(respuesta => respuesta.json()).then((data3) => { document.getElementById("BRL").innerHTML = (`1 USD vale ${ +(data3.brl).toFixed(2)} BRL`) })
+const fin2 = fetchUSDbrl(url3).then(res => res.json()).then((data3) => console.log(data3))
+fetchUSDbrl(url3).then(res => res.json()).then((data3) => { document.getElementById("BRL").innerHTML = (`1 USD vale ${ +(data3.brl).toFixed(2)} BRL`) })
 
 // USD-MEXICO
 
@@ -192,7 +94,7 @@ let url4 = [
     "https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/usd/mxn.json"
 ]
 
-
+fetchUSDmxn(url4).then(res => res.json()).then(data4 => { console.log(data4) })
 fetchUSDmxn(url4).then(res => res.json()).then(data4 => { document.getElementById("MXN").innerHTML = (`1 USD vale ${+(data4.mxn).toFixed(2)} MXN`) })
 
 // USD-Uruguayan
@@ -208,6 +110,8 @@ const fetchUSDuyu = async(links, obj) => {
     }
     return response
 }
+
+
 let url5 = [
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/uyu.min.json",
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/uyu.json",
@@ -215,4 +119,36 @@ let url5 = [
     "https://raw.githubusercontent.com/fawazahmed0/currency-api/1/latest/currencies/usd/uyu.json"
 ]
 
+fetchUSDuyu(url5).then(res => res.json()).then(data5 => console.log(data5))
 fetchUSDuyu(url5).then(res => res.json()).then(data5 => { document.getElementById("UYU").innerHTML = (`1 USD vale ${+(data5.uyu).toFixed(2)} UYU`) })
+
+
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['USD', 'EUR', 'CAD', 'BRL ', 'MNX', 'UYU'],
+        datasets: [{
+            data: [1, ],
+            label: '# of Votes',
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+
+});
